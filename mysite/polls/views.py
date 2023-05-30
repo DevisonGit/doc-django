51119401efac59444f03afa2d28d3d17bcca362c
Devison <devisonlobo@gmail.com>
2023-05-30 20:58:09 -0300

    Adicionando testes

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
-from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 
@@ -14,12 +14,15 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """return the last five publish questions"""
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultView(generic.DetailView):
